3f5bfbf45ebe2d94fbd8637f780a917d27a404e9
Colin Meinke <hello@colinmeinke.com>
2016-10-14 18:41:36 +0200

    chore(lint): switch to standard code style

--- src/index.js
+++ src/index.js
@@ -1,68 +1,68 @@
-import 'babel-polyfill';
+import 'babel-polyfill'
 
-import postcss from 'postcss';
+import postcss from 'postcss'
 
-const getAllSelectors = s => s.split( /\s{0,},\s{0,}/g );
+const getAllSelectors = s => s.split(/\s{0,},\s{0,}/g)
 
-const props = new Map();
+const props = new Map()
 
-const setProp = ( selector, prop, value ) => {
-  getAllSelectors( selector ).forEach( s => {
-    props.set( s, {
-      ...props.get( s ),
-      [ prop ]: value,
-    });
-  });
-};
+const setProp = (selector, prop, value) => {
+  getAllSelectors(selector).forEach(s => {
+    props.set(s, {
+      ...props.get(s),
+      [ prop ]: value
+    })
+  })
+}
 
-const getPropValue = ( selectors, prop ) => {
-  for ( const selector of selectors ) {
-    const selectorProps = props.get( selector );
+const getPropValue = (selectors, prop) => {
+  for (const selector of selectors) {
+    const selectorProps = props.get(selector)
 
-    if ( selectorProps && selectorProps[ prop ]) {
-      return selectorProps[ prop ];
+    if (selectorProps && selectorProps[ prop ]) {
+      return selectorProps[ prop ]
     }
   }
 
-  return 'undefined';
-};
+  return 'undefined'
+}
 
-const customProps = postcss.plugin( 'postcss-custom-props', () => css => {
-  css.walkRules( rule => {
-    rule.walkDecls( decl => {
-      const { prop, parent: { selector }} = decl;
+const customProps = postcss.plugin('postcss-custom-props', () => css => {
+  css.walkRules(rule => {
+    rule.walkDecls(decl => {
+      const { prop, parent: { selector } } = decl
 
-      let { value } = decl;
+      let { value } = decl
 
       // Value includes variable function call
-      if ( value.includes( 'var(' )) {
+      if (value.includes('var(')) {
         // Match var(...)'s
-        const matches = value.match( /var\([\w\- ]+\)/g );
+        const matches = value.match(/var\([\w\- ]+\)/g)
 
-        matches.forEach( match => {
+        matches.forEach(match => {
           // Get var(...)'s prop
-          const matchedProp = match.slice( 4, -1 ).trim();
+          const matchedProp = match.slice(4, -1).trim()
 
           // Get stored value of prop
           const propValue = getPropValue([
-            ...getAllSelectors( selector ),
-            ':root',
-          ], matchedProp );
+            ...getAllSelectors(selector),
+            ':root'
+          ], matchedProp)
 
           // Replace var(...) with prop value
-          decl.value = value = value.replace( match, propValue ); // eslint-disable-line no-param-reassign,max-len
-        });
+          decl.value = value = value.replace(match, propValue) // eslint-disable-line no-param-reassign,max-len
+        })
       }
 
       // Prop includes custom property definition
-      if ( prop.startsWith( '--' )) {
+      if (prop.startsWith('--')) {
         // Add prop to map
-        setProp( selector, prop, value );
+        setProp(selector, prop, value)
         // Remove style declaration
-        decl.remove();
+        decl.remove()
       }
-    });
-  });
-});
+    })
+  })
+})
 
-export default customProps;
+export default customProps
--- test/index.js
+++ test/index.js
@@ -1,100 +1,102 @@
-import expect from 'expect';
-import postcss from 'postcss';
+/* eslint-env mocha */
 
-import customProps from '../src';
+import expect from 'expect'
+import postcss from 'postcss'
 
-const process = css => postcss([ customProps() ]).process( css );
+import customProps from '../src'
 
-describe( 'custom property definition', () => {
-  it( 'should replace `var(--foo)` value with custom property', () => {
-    const input = ':root { --foo: red; --bar: var(--foo) } h1 { color: var(--bar); }';
-    const expectedOutput = 'h1 { color: red; }';
+const process = css => postcss([ customProps() ]).process(css)
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+describe('custom property definition', () => {
+  it('should replace `var(--foo)` value with custom property', () => {
+    const input = ':root { --foo: red; --bar: var(--foo) } h1 { color: var(--bar); }'
+    const expectedOutput = 'h1 { color: red; }'
 
-  it( 'should remove the definition from output', () => {
-    const input = ':root { --foo: red; }';
-    const expectedOutput = ':root { }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toEqual( expectedOutput );
-    });
-  });
-});
+  it('should remove the definition from output', () => {
+    const input = ':root { --foo: red; }'
+    const expectedOutput = ':root { }'
 
-describe( 'rule', () => {
-  it( 'should replace `var(--foo)` value with custom property', () => {
-    const input = ':root { --foo: red; } h1 { color: var(--foo); }';
-    const expectedOutput = 'h1 { color: red; }';
+    return process(input).then(({ css }) => {
+      expect(css).toEqual(expectedOutput)
+    })
+  })
+})
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+describe('rule', () => {
+  it('should replace `var(--foo)` value with custom property', () => {
+    const input = ':root { --foo: red; } h1 { color: var(--foo); }'
+    const expectedOutput = 'h1 { color: red; }'
 
-  it( 'should replace `var(--foo) var(--bar)` value with multiple custom properties', () => {
-    const input = ':root { --foo: 20px; --bar: 10px; } h1 { padding: var(--foo) var(--bar); }';
-    const expectedOutput = 'h1 { padding: 20px 10px; }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+  it('should replace `var(--foo) var(--bar)` value with multiple custom properties', () => {
+    const input = ':root { --foo: 20px; --bar: 10px; } h1 { padding: var(--foo) var(--bar); }'
+    const expectedOutput = 'h1 { padding: 20px 10px; }'
 
-  it( 'should only replace `var(--foo)` from `color(var(--foo) l(25%))` value', () => {
-    const input = ':root { --foo: red; } h1 { color: color(var(--foo) l(25%)); }';
-    const expectedOutput = 'h1 { color: color(red l(25%)); }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+  it('should only replace `var(--foo)` from `color(var(--foo) l(25%))` value', () => {
+    const input = ':root { --foo: red; } h1 { color: color(var(--foo) l(25%)); }'
+    const expectedOutput = 'h1 { color: color(red l(25%)); }'
 
-  it( 'should replace `var( --foo )` value that includes whitespace', () => {
-    const input = ':root { --foo: red; } h1 { color: var( --foo ); }';
-    const expectedOutput = 'h1 { color: red; }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+  it('should replace `var( --foo )` value that includes whitespace', () => {
+    const input = ':root { --foo: red; } h1 { color: var( --foo ); }'
+    const expectedOutput = 'h1 { color: red; }'
 
-  it( 'should replace `var(--foo-bar)` value that is kebab-case', () => {
-    const input = ':root { --foo-bar: red; } h1 { color: var( --foo-bar ); }';
-    const expectedOutput = 'h1 { color: red; }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+  it('should replace `var(--foo-bar)` value that is kebab-case', () => {
+    const input = ':root { --foo-bar: red; } h1 { color: var( --foo-bar ); }'
+    const expectedOutput = 'h1 { color: red; }'
 
-  it( 'should replace `var(--foo_bar)` value that is snake-case', () => {
-    const input = ':root { --foo_bar: red; } h1 { color: var( --foo_bar ); }';
-    const expectedOutput = 'h1 { color: red; }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+  it('should replace `var(--foo_bar)` value that is snake-case', () => {
+    const input = ':root { --foo_bar: red; } h1 { color: var( --foo_bar ); }'
+    const expectedOutput = 'h1 { color: red; }'
 
-  it( 'should replace `var(--foo)` value after definition on seperate multi-selector rule', () => {
-    const input = 'h1, h2 { --foo: red; } h2 { color: var(--foo); }';
-    const expectedOutput = 'h2 { color: red; }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
+  it('should replace `var(--foo)` value after definition on seperate multi-selector rule', () => {
+    const input = 'h1, h2 { --foo: red; } h2 { color: var(--foo); }'
+    const expectedOutput = 'h2 { color: red; }'
 
-  it( 'should replace `var(--foo)` value in multi-selector rule after seperate definition', () => {
-    const input = 'h1 { --foo: red; } h1, h2 { color: var(--foo); }';
-    const expectedOutput = 'h1, h2 { color: red; }';
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
 
-    return process( input ).then(({ css }) => {
-      expect( css ).toInclude( expectedOutput );
-    });
-  });
-});
+  it('should replace `var(--foo)` value in multi-selector rule after seperate definition', () => {
+    const input = 'h1 { --foo: red; } h1, h2 { color: var(--foo); }'
+    const expectedOutput = 'h1, h2 { color: red; }'
+
+    return process(input).then(({ css }) => {
+      expect(css).toInclude(expectedOutput)
+    })
+  })
+})
